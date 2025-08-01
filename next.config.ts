53152cb49febcca043f4ad9f9e20f82d4a32cc4a
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-01 12:46:56 +0100

    commits

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enables static export
+  basePath: isProd ? `/nextjsApp` : "",
+  assetPrefix: isProd ? `/nextjsApp/` : "",
 };
 
 export default nextConfig;
