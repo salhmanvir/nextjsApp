4509e5dccb45cff8b23550d139786c68b642df90
salhmanvir <Manvir.Salh@kpmg.co.uk>
2025-08-01 14:07:20 +0100

    imag eoptimisation config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
   output: "export", // Enables static export
   basePath: isProd ? `/nextjsApp` : "",
   assetPrefix: isProd ? `/nextjsApp/` : "",
